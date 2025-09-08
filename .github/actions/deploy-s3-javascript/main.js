9903d8ddd9be9a1ea9b595da9688af9f92e2ece4
BenjaminAtrium <blory@atrium-capital.com>
2025-09-08 09:25:25 +0200

    added custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,16 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 const run = () => {
-  core.notice("Hello from my custom Javascript action!");
+  // 1) Get some input values
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
+
+  // 2) Upload files
+  const s3Uri = `s3://${bucket}`;
+  // note: AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables are needed to use AWS cli command below
+  // exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+  core.notice(`use "aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}" to upload files on AWS S3 bucket`);
 };
 
 run();
